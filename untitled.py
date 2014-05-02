8acbb1383f6379f756524c5cd3ea5d1437490d99
toshyak <osheychik@gmail.com>
2014-05-02 12:40:54 +0400

    Авторизация и обработка exception при открытии профиля

--- untitled.py
+++ untitled.py
@@ -1,7 +1,19 @@
-import urllib
+import urllib2
 import hashlib
 
 my_password='6$1KnH0xARI3VdI3=;6Q!h|r"'
-username = 'toshyak'
+username = 'Toshyak'
 
 my_password_md5 = hashlib.md5(my_password)
+
+print "http://api.myshows.ru/profile/login?login=%(login)s&password=%(passwd)s" % {"login" :username, "passwd": my_password_md5.hexdigest()}
+auth = urllib2.urlopen("http://api.myshows.ru/profile/login?login=%(login)s&password=%(passwd)s " % \
+	{"login":username, "passwd": my_password_md5.hexdigest()} )
+
+print auth.code
+try:
+	profile = urllib2.urlopen("http://api.myshows.ru/profile/ ")
+except urllib2.HTTPError as err:
+	print err
+
+
