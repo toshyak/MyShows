b93e62a01910537c20f29161a1166e75ccecb366
toshyak <osheychik@gmail.com>
2014-05-04 00:00:29 +0400

    Work with cookies

--- untitled.py
+++ untitled.py
@@ -1,4 +1,4 @@
-import urllib2
+import urllib2, cookielib
 import hashlib
 
 my_password='6$1KnH0xARI3VdI3=;6Q!h|r"'
@@ -6,11 +6,17 @@ username = 'Toshyak'
 
 my_password_md5 = hashlib.md5(my_password)
 
-print "http://api.myshows.ru/profile/login?login=%(login)s&password=%(passwd)s" % {"login" :username, "passwd": my_password_md5.hexdigest()}
-auth = urllib2.urlopen("http://api.myshows.ru/profile/login?login=%(login)s&password=%(passwd)s " % \
+# print "http://api.myshows.ru/profile/login?login=%(login)s&password=%(passwd)s" % {"login" :username, "passwd": my_password_md5.hexdigest()}
+cj=cookielib.CookieJar()
+opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
+auth = opener.open("http://api.myshows.ru/profile/login?login=%(login)s&password=%(passwd)s " % \
 	{"login":username, "passwd": my_password_md5.hexdigest()} )
 
-print auth.code
+print auth.getcode()
+
+for index, cookie in enumerate(cj):
+        print index, '  :  ', cookie
+
 try:
 	profile = urllib2.urlopen("http://api.myshows.ru/profile/ ")
 except urllib2.HTTPError as err:
